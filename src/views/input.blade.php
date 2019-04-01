d4a2034457326a873f6d078a38d0d8c694f2a025
Leonardo <leonardo_pardo@hotmail.com>
2019-03-31 23:25:12 -0300

    Fix errros messages.

--- src/views/input.blade.php
+++ src/views/input.blade.php
@@ -1,17 +1,17 @@
 <input 
-type="{{ $type ?? 'text' }}" 
-id="{{ $id ?? '' }}"
-name="{{ $name }}"
-class="form-control {{ $class ?? '' }}"
-value="{{ $value ?? ''}}"
-placeholder="{{ $placeholder ?? '' }}"
-@isset($maxlength)
-maxlength={{$maxlength}}
-@endisset
-@isset($minlength)
-minlength={{$minlength}}
-@endisset
-@if(isset($required) && $required)
-required
-@endif
-{{ $extra ?? ''}}>
+    type="{{ $type ?? 'text' }}" 
+    id="{{ $id ?? '' }}" 
+    name="{{ $name }}" 
+    class="form-control {{ $class ?? '' }}" 
+    value="{{ old($name, $value ?? '') }}"
+    placeholder="{{ $placeholder ?? '' }}"
+    @isset($maxlength)
+        maxlength={{$maxlength}}
+    @endisset
+    @isset($minlength)
+        minlength={{$minlength}}
+    @endisset
+    @if(isset($required) && $required)
+        required
+    @endif
+    {{ $extra ?? ''}} >
