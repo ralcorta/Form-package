71495592d4c9499c95789a79f0c48e2f992f3457
Rodrigo <rodrigovictoralcorta@gmail.com>
2019-04-01 13:12:35 -0300

    Checkout fixed

--- src/views/radio_checkbox.blade.php
+++ src/views/radio_checkbox.blade.php
@@ -1,10 +1,14 @@
-<div class="form-check 
-    @isset($errors) {{ $errors->has($name) ? 'has-error' : '' }} @endisset() 
-    {{ $class ?? '' }}" 
-    @if(isset($checked) && $checked) checked @endif
-    >
-    <input class="form-check-input" type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" @if(isset($checked) && $checked) checked @endif>
-    <label class="form-check-label" for="{{ $id }}">
-        {{ $text }}
-    </label>
+<div 
+    class="form-check custom-control custom-checkbox @isset($errors) {{ $errors->has($name) ? 'has-error' : '' }} @endisset(){{ $class['div'] ?? '' }}" 
+    @if(isset($checked) && $checked) checked @endif>
+    <input 
+        class="form-check-input custom-control-input"
+        type="{{ $type }}" 
+        name="{{ $name }}" 
+        id="{{ $id }}" 
+        @isset($value) value="{{ $value }}" @endisset 
+        @if(isset($checked) && $checked) checked @endif>
+    <label 
+    class="form-check-label custom-control-label @isset($class)@if(isset($class['label'])){{ $class['label'] }}@elseif(!isset($class['div'])){{ $class }}@endif @endisset" 
+    for="{{ $id }}">{{ $text }}</label>
 </div>
