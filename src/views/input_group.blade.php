43c2991d6f1742619dd0e725bcd6e059f25d55cb
Rodrigo <rodrigovictoralcorta@gmail.com>
2019-08-27 22:42:41 -0300

    Required (*)

--- src/views/input_group.blade.php
+++ src/views/input_group.blade.php
@@ -1,6 +1,11 @@
 <div class="form-group @isset($errors){{ $errors->has($name) ? 'has-error' : '' }}@endisset()" >
     @isset($label)
-    <label for="{{ $id ?? '' }}">{{ $label }}</label>
+    <label for="{{ $id ?? '' }}">
+        {{ $label }}
+        @if(isset($required) && $required)
+        <b>*</b>
+        @endif
+    </label>
     @endisset()
 
     @include('form::input')
